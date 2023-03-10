ec1050148f53850fea9ad018e44f36bdee8964cc
Meick Mora <meickmora9@gmail.com>
2023-03-10 20:34:44 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.send("Hello World")
+})
+
+app.listen(3000, () => console.log('Server is running'));

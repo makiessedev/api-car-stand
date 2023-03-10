e439f206e0c6285a13698fc67434eabb9a8ea402
Meick Mora <meickmora9@gmail.com>
2023-03-10 20:41:48 +0100

    added route courses

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello World")
-})
+app.get('/', (request, response) => {
+  return response.send('Hello World');
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
 
 app.listen(3000, () => console.log('Server is running'));
